41da6e65880d51e39babcbbdd31d4d9dbc15e684
davidli898 <baolianbao@gmail.com>
2017-03-22 23:47:53 +0800

    Day09

--- Day08/ex15/ex15.py
+++ Day08/ex15/ex15.py
@@ -1,12 +1,11 @@
 from sys import argv
+
 script, filename = argv
+#line 1-3 should be familiar use of argv to get a filename.
+txt = open(filename) # "open" is a new command , Pydoc said:   a file using the file() type, returns a file object.  This is the preferred way to open a file.
 
-txt = open(filename) # "open"    a file using the file() type, returns a file object.  This is the preferred way to open a file.
-
-
-print "Here's your file %r:" % filename
-print txt.read()
-
+print "Here's your file %r:" % filename # print  a  little line,include the filename that you just typed.
+print txt.read() # on line 8 we hdve something very new and exciting. It 's a function on txt, You give a filename by use . (dot or period), the name of the command, and parameters.Just like "open" and "raw_input".
 print "Type the filename again:"
 file_again = raw_input("> ")
 # use raw_input get filename again
