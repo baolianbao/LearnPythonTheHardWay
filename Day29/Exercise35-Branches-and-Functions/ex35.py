ecaf4704fc0c836e85338467e3662f3ea00ec410
David <David@xianzideiMac.local>
2017-04-15 23:19:32 +0800

    Day33

--- Day29/Exercise35-Branches-and-Functions/ex35.py
+++ Day29/Exercise35-Branches-and-Functions/ex35.py
@@ -31,9 +31,9 @@ def bear_room():
         elif next == "taunt bear" and not bear_moved:
             print "The bear has moved from the door. You can go through it now."
             bear_moved = True
-        elif next == "tanut bear" and bear_moved:
+        elif next == "taunt bear" and bear_moved:
             dead("The bear gets pissed off and chews your leg off.")
-        elif next == "oprn door" and bear_moved:
+        elif next == "open door" and bear_moved:
             gold_room()
         else:
             print " I got no idea what that means."
