8c58650bc3920fc2e669e9e6a4e42d7aa5701c00
davidli898 <baolianbao@gmail.com>
2017-05-05 00:22:24 +0800

    Day51

---
+++ Day51/Exercise44-Inheritance-vs-Composition/ex44.py
@@ -0,0 +1,13 @@
+class Parent(object):
+
+    def implicit(self):
+        print "PARENT implicit()"
+
+class Child(Parent):
+    pass
+
+dad = Parent()
+son = child()
+
+dad.implicit()
+son.implicit()
